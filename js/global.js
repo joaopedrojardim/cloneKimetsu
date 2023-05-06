a315219b380babd30cc29ac9d22f0ed67be8e725
joaopedrojardim <joaopedrocjardim@gmail.com>
2023-05-06 16:01:54 -0300

    Teste

--- js/global.js
+++ js/global.js
@@ -1,7 +1,7 @@
 import ModalVideo from "./video.js";
 import Tooltip from "./mousehover.js";
 
-const modal = new ModalVideo('[data-modal="abrir"]', '[data-modal="fechar"]', '[data-modal="container"]'    )
+const modal = new ModalVideo('[data-modal="abrir"]', '[data-modal="fechar"]', '[data-modal="container"]')
 modal.init()
 
 const tooltip = new Tooltip('[data-hover]')
